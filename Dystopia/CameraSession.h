b9cb76f79c77a04cffac6b8a5f3b212466a8d8cd
black-knight <dani_ande@yahoo.dk>
2013-07-08 19:51:45 +0200

    App icons

---
+++ Dystopia/CameraSession.h
@@ -0,0 +1,13 @@
+//
+//  Camera.h
+//  Dystopia
+//
+//  Created by Daniel Andersen on 7/7/13.
+//  Copyright (c) 2013 Daniel Andersen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Camera : NSObject
+
+@end
---
+++ Dystopia/CameraSession.m
@@ -0,0 +1,13 @@
+//
+//  Camera.m
+//  Dystopia
+//
+//  Created by Daniel Andersen on 7/7/13.
+//  Copyright (c) 2013 Daniel Andersen. All rights reserved.
+//
+
+#import "Camera.h"
+
+@implementation Camera
+
+@end
